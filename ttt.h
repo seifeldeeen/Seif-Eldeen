50bd2305f52c31b9bd625bad1ed4e1eb537994df
Seif Eldeen <112936882+seifeldeeen@users.noreply.github.com>
2024-12-29 22:21:33 +0200

    Create ttt.h

---
+++ ttt.h
@@ -0,0 +1,12 @@
+
+#include <vector>
+void greet();
+void draw();
+bool fill();
+bool is_winner();
+void set();
+void update_draw();
+void take_turn();
+void end_game();
+void change_player();
+void end_game();
